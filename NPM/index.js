8aad5ea467dda47f7015079fdfbd758162741078
NAMAN JAIN <namanjain152003@gmail.com>
2025-04-14 15:08:50 +0000

    NPM usage

---
+++ NPM/index.js
@@ -0,0 +1,26 @@
+//var generateName = require('sillyname');
+
+/*import generateName  from "sillyname"
+var sillyName = generateName();
+console.log(`my name is ${sillyName} `);*/
+
+/*import {randomsuperhero} from "superheroes";
+const name = randomsuperhero();
+console.log(`my name is ${name}!`);*/
+/*
+import superheroes from 'superheroes';
+
+const name = superheroes.random();
+console.log(`My name is ${name}!`);
+*/
+/*import * as superheroes from 'superheroes';
+
+const name = superheroes.random();
+console.log(`My name is ${name}!`);
+
+*/
+
+import {randomSuperhero} from 'superheroes';
+
+ let name = randomSuperhero();
+ console.log(`My name is ${name}!`);
